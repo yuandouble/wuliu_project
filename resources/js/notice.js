84255ad640a3a3ea99a35fda618b1dd58652a80b
yuanshuang <2317909384@qq.com>
2018-05-29 15:03:59 +0800

    入场须知页面编辑

---
+++ resources/js/notice.js
@@ -0,0 +1,20 @@
+$(function(){
+	$("#container").html("hello????来尝试下哈哈");
+
+	//点击修改的按钮，切换到可编辑状态；
+	$("#notice_edit_btn").on("click",function(){
+
+	});
+	//点击发布按钮，进行发布；
+	$("#notice_publish_btn").on("click",function(){
+
+	});
+	//点击保存按钮，进行保存
+	$("#notice_store_btn").on("click",function(){
+
+	});
+	//点击取消按钮，切换到不可编辑状态
+	$("#notice_cancel_btn").on("click",function(){
+
+	});
+});
