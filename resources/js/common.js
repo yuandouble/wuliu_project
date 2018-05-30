248e693e4398bb46be4786956413d307835ad4b5
zhangyanmin <zym931126@163.com>
2018-05-30 17:33:07 +0800

    代码提交

--- resources/js/common.js
+++ resources/js/common.js
@@ -7,9 +7,14 @@ var common_frame = {
 	}
 }
 
-//时间格式化
+
 var commons = {
+    //时间格式化
     timeFormat: function (time) {
         return time.substr(0, 4) + '-' + time.substr(4, 2) + '-' + time.substr(6, 2);
+    },
+	// 去除空格
+    deleteSpace:function (para) {
+        return para.val().replace(/\s/g, "")
     }
 }
